90447e7928ab68992f3db6e1e1b07eee905781a5
Tomas Allami <totoallami@gmail.com>
2022-07-03 20:05:44 -0300

    Cambio argsparse por sysargs

--- listado_chesques.py
+++ listado_chesques.py
@@ -33,17 +33,14 @@ with open (f"{nombre_csv}", "r") as archivo:
               print(line)
             elif estado == None:
               print(line)
-
-                  
+                 
         elif salida == "CSV":
-          if tipo in line[9]:
-            if estado in line and tipo in line[9]:
-              with open(f"{dni}-{dtn}.csv", "a") as archivo:
-                csv_writer = csv.writer(archivo)
-                csv_writer.writerow(line) 
-            elif estado == None and tipo in line[9]: 
-              with open(f"{dni}-{dtn}.csv", "a") as archivo:
-                csv_writer = csv.writer(archivo)
-                csv_writer.writerow(line)
+          with open(f"{dni}-{dtn}.csv", "a") as archivo:
+            csv_writer = csv.writer(archivo)           
+            if tipo in line[9]:
+              if estado in line and tipo in line[9]:
+                  csv_writer.writerow(line) 
+              elif estado == None and tipo in line[9]: 
+                  csv_writer.writerow(line)
 
         
