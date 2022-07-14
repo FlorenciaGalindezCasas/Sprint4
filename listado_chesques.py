7a9b2267d29afb2190c373d80dbcca1c7571a8fb
Tomas Allami <totoallami@gmail.com>
2022-07-14 11:20:29 -0300

    script terminado

--- listado_chesques.py
+++ listado_chesques.py
@@ -1,10 +1,7 @@
 import csv
 import sys
 from sys import argv
-from datetime import datetime
-
-dt = datetime.now()
-dtn = dt.strftime("%d-%m-%Y")
+from datetime import date, datetime
 
 n = len(sys.argv)
 
@@ -16,31 +13,62 @@ estado = None
 fecha = None
 
 if n == 6:
-  estado = sys.argv[5]
-  fecha = sys.argv[5]
+  opcional = sys.argv[5]
+  estados = ["PENDIENTE", "APROBADO", "RECHAZADO"]
+  if opcional in estados:
+    estado = opcional
+  else:
+    fecha = opcional.split(":")
+
 elif n == 7:
   estado = sys.argv[5]
-  fecha = sys.argv[6]
+  fecha = sys.argv[6].split(":")
+
+if fecha:
+  fecha_inicial = datetime.timestamp(datetime.strptime(fecha[0], "%d-%m-%y"))
+  fecha_final = datetime.timestamp(datetime.strptime(fecha[1], "%d-%m-%y"))
+  
+res = []
 
 with open (f"{nombre_csv}", "r") as archivo:
-    csv_reader = csv.reader(archivo)
+    csv_reader = csv.reader(archivo, delimiter=",")
     
-    for line in csv_reader:   
-      if dni in line[8]:      
-        if salida == "PANTALLA":
-          if tipo in line:
-            if estado in line and tipo in line:
-              print(line)
-            elif estado == None:
-              print(line)
-                 
-        elif salida == "CSV":
-          with open(f"{dni}-{dtn}.csv", "a") as archivo:
-            csv_writer = csv.writer(archivo)           
-            if tipo in line[9]:
-              if estado in line and tipo in line[9]:
-                  csv_writer.writerow(line) 
-              elif estado == None and tipo in line[9]: 
-                  csv_writer.writerow(line)
+    for line in csv_reader:
+      dni_cheque = line[8]
+      tipo_cheque = line[9]
+      estado_cheque = line[10]  
+      fecha_cheque = line[6]
+       
+      if dni_cheque != dni or tipo_cheque != tipo:
+        continue
+      if estado and estado_cheque != estado:
+        continue      
+      if fecha and (fecha_cheque < fecha_inicial or fecha_cheque > fecha_final):
+        continue
+      res.append(line)
+ 
+filtrados = set()     
+for nro_line, line in enumerate(res):
+   nro_cheque = line[0]
+   nro_cuenta = line[3]
+   nro_dni = line[8]
+
+if(nro_cheque, nro_cuenta, nro_dni) in filtrados:
+  res.append(f"Hay datos repetidos en fila {nro_line}")
+else:
+  filtrados.add((line[0], line[3], line[8]))
+                       
+if salida == "PANTALLA":
+  for fila in res:
+    print(",".join(fila))
+    
+elif salida == "CSV":
+    datos = [[line[3], line[5], line[6], line[7]]for line in res]
+    dt = datetime.now()
+    dt = dt.strftime("%d,%m,%Y")
+    with open(f"{dni}-{dt}.csv", "w", newline="") as archivo:
+        writer = csv.writer(archivo)
+        writer.writerows(datos)
+  
 
         
